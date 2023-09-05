a10a5db71fe226da1423bf5b6a1302c88e2636c6
sunnununii1 <134986553+sunnununii1@users.noreply.github.com>
2023-09-05 18:12:07 +0900

    Room.java : 룸메인 서블릿

--- servlet/semi_project/src/main/java/kr/kh/app/controller/Room.java
+++ servlet/semi_project/src/main/java/kr/kh/app/controller/Room.java
@@ -9,17 +9,23 @@ import java.io.IOException;
 
 public class Room extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private RoomService roomService = new RoomServiceImp();
 
     public Room() {
         super();
     }
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//		int ro_num = Integer.parseInt(request.getParameter("ro_num"));
+		ArrayList<RoomVO> list = roomService.getRoomList();
+		request.setAttribute("list", list);
+		
 		request.getRequestDispatcher("/WEB-INF/views/room/roommain.jsp").forward(request, response);
-	}
+}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
+	
 
 }
