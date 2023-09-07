38bda9fdf3e87cabe4db921e185502f17e0b877a
JOMINJEONGG <chldbwls8974@naver.com>
2023-09-07 17:47:57 +0900

    수정함

--- servlet/semi_project/src/main/java/kr/kh/app/controller/MyPage.java
+++ servlet/semi_project/src/main/java/kr/kh/app/controller/MyPage.java
@@ -1,17 +1,24 @@
 package kr.kh.app.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import jakarta.servlet.http.HttpSession;
+import kr.kh.app.service.DogService;
+import kr.kh.app.service.DogServiceImp;
+import kr.kh.app.vo.DogVO;
+import kr.kh.app.vo.MemberVO;
 
 /**
  * Servlet implementation class MyPage
  */
 public class MyPage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	private DogService dogService = new DogServiceImp();
        
     /**
      * @see HttpServlet#HttpServlet()
@@ -25,6 +32,12 @@ public class MyPage extends HttpServlet {
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
+		MemberVO user = (MemberVO)session.getAttribute("user"); 
+		
+		ArrayList<DogVO> list = dogService.getMyDogList(user);
+		request.setAttribute("list", list);
+		
 		request.getRequestDispatcher("/WEB-INF/views/member/mypage.jsp").forward(request,response);
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
