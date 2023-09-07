72644532a4b28b42dc22859964b5bc9578e77ef9
JOMINJEONGG <chldbwls8974@naver.com>
2023-09-07 16:27:22 +0900

    회원 조회 기능 추가

--- servlet/semi_project/src/main/java/kr/kh/app/controller/MemberList.java
+++ servlet/semi_project/src/main/java/kr/kh/app/controller/MemberList.java
@@ -1,39 +1,32 @@
 package kr.kh.app.controller;
 
+import java.io.IOException;
+import java.util.ArrayList;
+
 import jakarta.servlet.ServletException;
-import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import java.io.IOException;
+import kr.kh.app.service.MemberService;
+import kr.kh.app.service.MemberServiceImp;
+import kr.kh.app.vo.MemberVO;
 
-/**
- * Servlet implementation class MemberList
- */
 public class MemberList extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
+    private MemberService memberService = new MemberServiceImp();  
+	
     public MemberList() {
         super();
-        // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		ArrayList<MemberVO> list = memberService.selectMemberList();
+		request.setAttribute("list", list);
 		request.getRequestDispatcher("/WEB-INF/views/member/list.jsp").forward(request, response);	
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
 		doGet(request, response);
 	}
 
--- servlet/semi_project/src/main/webapp/WEB-INF/views/member/list.jsp
+++ servlet/semi_project/src/main/webapp/WEB-INF/views/member/list.jsp
@@ -9,25 +9,27 @@
 </head>
 <body>
 	<jsp:include page="/WEB-INF/views/header.jsp"></jsp:include>
-	<table class="table table-hover">
-	    <thead>
-	      <tr>
-	        <th>아이디</th>
-	        <th>이름</th>
-	        <th>주소</th>
-	        <th>번호</th>
-	      </tr>
-	    </thead>
-	    <tbody>
-	    	<c:forEach items="${list}" var="branch">
+	<div class="container mt-5">
+		<table class="table table-hover">
+		    <thead>
 		      <tr>
-		        <td>${user.me_id}</td>
-		        <td>${user.me_name}</td>
-		        <td>${user.address}</td>
-		        <td>${user.me_phone}</td>
+		        <th>아이디</th>
+		        <th>이름</th>
+		        <th>주소</th>
+		        <th>번호</th>
 		      </tr>
-		    </c:forEach>
-	    </tbody>
-	</table>
+		    </thead>
+		    <tbody>
+		    	<c:forEach items="${list}" var="member">
+			      <tr>
+			        <td>${member.me_id}</td>
+			        <td>${member.me_name}</td>
+			        <td>${member.me_address}</td>
+			        <td>${member.me_phone}</td>
+			      </tr>
+			    </c:forEach>
+		    </tbody>
+		</table>
+	</div>
 </body>
 </html>
