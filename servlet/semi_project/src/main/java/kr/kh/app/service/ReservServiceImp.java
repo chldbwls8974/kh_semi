9717cf4cbe72f7fbbce55133a73644b9b7d3c180
ekzot1212 <ekzot1212@gmail.com>
2023-09-20 19:51:54 +0900

    .

--- servlet/semi_project/src/main/java/kr/kh/app/service/ReservServiceImp.java
+++ servlet/semi_project/src/main/java/kr/kh/app/service/ReservServiceImp.java
@@ -163,11 +163,16 @@ public class ReservServiceImp implements ReservService{
 
 
 	@Override
-	public boolean isCorrectSelect(String br_num, String[] dataArray, String[] roomArray) {
+	public boolean isCorrectSelect(String br_num, String[] dogArray, String[] roomArray) {
 		if(br_num == null || br_num == "0") {
 			return false;
 		}
-		for(String tmp : dataArray) {
+		for(String tmp : dogArray) {
+			if(tmp.equals("0") || tmp == null) {
+				return false;
+			}
+		}
+		for(String tmp : roomArray) {
 			if(tmp.equals("0") || tmp == null) {
 				return false;
 			}
