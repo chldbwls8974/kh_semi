7d83898878d043253a25151e9a2a172cd98219dd
ekzot1212 <ekzot1212@gmail.com>
2023-09-26 17:50:29 +0900

    db

---
+++ ddl/0926/dogtel_reservationlist.sql
@@ -0,0 +1,61 @@
+CREATE DATABASE  IF NOT EXISTS `dogtel` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `dogtel`;
+-- MySQL dump 10.13  Distrib 8.0.34, for Win64 (x86_64)
+--
+-- Host: localhost    Database: dogtel
+-- ------------------------------------------------------
+-- Server version	8.0.34
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `reservationlist`
+--
+
+DROP TABLE IF EXISTS `reservationlist`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `reservationlist` (
+  `rl_num` int NOT NULL AUTO_INCREMENT,
+  `rl_d_num` varchar(13) NOT NULL,
+  `rl_ro_num` int NOT NULL,
+  `rl_re_num` varchar(50) NOT NULL,
+  PRIMARY KEY (`rl_num`),
+  KEY `FK_room_TO_reservationList_1` (`rl_ro_num`),
+  KEY `re_to_rl_idx` (`rl_re_num`),
+  KEY `dog_to_rl_idx` (`rl_d_num`),
+  CONSTRAINT `dog_to_rl` FOREIGN KEY (`rl_d_num`) REFERENCES `dog` (`d_num`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `FK_room_TO_reservationList_1` FOREIGN KEY (`rl_ro_num`) REFERENCES `room` (`ro_num`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `re_to_rl` FOREIGN KEY (`rl_re_num`) REFERENCES `reservation` (`re_num`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=66 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `reservationlist`
+--
+
+LOCK TABLES `reservationlist` WRITE;
+/*!40000 ALTER TABLE `reservationlist` DISABLE KEYS */;
+INSERT INTO `reservationlist` VALUES (62,'test0926001',11,'test09262023-09-27test0926001');
+/*!40000 ALTER TABLE `reservationlist` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2023-09-26 17:49:46
